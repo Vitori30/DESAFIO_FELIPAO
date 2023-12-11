f63aaad6f7717b04a94db53da1e884b621b73945
Vitória <vitoria9030@gmail.com>
2023-12-11 18:45:56 -0300

    PRIMEIRO COMMIT

---
+++ aula/index.js
@@ -0,0 +1,64 @@
+//projeto de lógica
+console.log ("DESAFIO CLASSIFICADOR DE NIVEL DE HEROI ")
+//saida
+console.log ("Digita o nome do seu jogador")
+
+//declara uma variável
+let nickname = " Valente jogadora"
+
+//concantenando uma mensagem fixa + uma váriavel 
+console.log ("Bem Vinda " + nickname)
+console.log (nickname  +  " entrou no servidor")
+
+const notificacao = "Pokemon Go diz:"
+//sainda de mensagens
+console.log (notificacao +  "tem um novo pokemon na região")
+console.log (notificacao +  "Você foi derrotado por um líder")
+//pokemon
+let nomepokemon ="Pikachu"
+let pokemonsexo = "M"
+let selecionavel= true
+let (XP)="XP"
+
+let Ferro= "Ferro"
+let Bronze= "Bronze"
+let Prata= "Prata"
+let Ouro= "Ouro"
+let Platina= "Platina"
+let Ascendente= "Ascendente"
+let Imortal= "Imortal"
+let Radiante= "Radiante"
+
+
+console.log (Ferro)+ "<1000" +
+console.log (Bronze) + ">1.000 && <2.001"
+console.log (Prata) +"> 2.001 && >5.000"+
+console.log(Ouro)+">6.001 && <7.000"+
+console.log (Platina) +">7.001 && <8.000"+
+console.log (Ascendente)+">8.001 && <9.000"+
+console.log (Imortal) +">9.001 && <10.000"+
+console.log (Radiante) +">= 10.001"+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//saida
+console.log ("O herói de nome" + nickname + "está no nível de"  )
