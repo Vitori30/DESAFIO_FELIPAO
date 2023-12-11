8f5ecb08f2a194a2fbcc5843117935fce187e222
Vitori30 <152215320+Vitori30@users.noreply.github.com>
2023-12-11 19:02:44 -0300

    Teste index.js

--- aula/index.js
+++ aula/index.js
@@ -1,3 +1,4 @@
+Testando
 //projeto de lógica
 console.log ("DESAFIO CLASSIFICADOR DE NIVEL DE HEROI ")
 //saida
@@ -16,49 +17,3 @@ console.log (notificacao +  "tem um novo pokemon na região")
 console.log (notificacao +  "Você foi derrotado por um líder")
 //pokemon
 let nomepokemon ="Pikachu"
-let pokemonsexo = "M"
-let selecionavel= true
-let (XP)="XP"
-
-let Ferro= "Ferro"
-let Bronze= "Bronze"
-let Prata= "Prata"
-let Ouro= "Ouro"
-let Platina= "Platina"
-let Ascendente= "Ascendente"
-let Imortal= "Imortal"
-let Radiante= "Radiante"
-
-
-console.log (Ferro)+ "<1000" +
-console.log (Bronze) + ">1.000 && <2.001"
-console.log (Prata) +"> 2.001 && >5.000"+
-console.log(Ouro)+">6.001 && <7.000"+
-console.log (Platina) +">7.001 && <8.000"+
-console.log (Ascendente)+">8.001 && <9.000"+
-console.log (Imortal) +">9.001 && <10.000"+
-console.log (Radiante) +">= 10.001"+
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//saida
-console.log ("O herói de nome" + nickname + "está no nível de"  )
